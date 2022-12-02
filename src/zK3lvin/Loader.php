e188799973809ccbc6ff05fc9c9b1ff16a7bc20e
zK3lvin <109186535+zK3lvin@users.noreply.github.com>
2022-12-01 22:22:19 -0600

    update Loader.php

--- src/zK3lvin/Loader.php
+++ src/zK3lvin/Loader.php
@@ -1,1 +1,16 @@
+<?php
 
+namespace zK3lvin;
+
+use pocketmine\plugin\PluginBase;
+use zK3lvin\command\BroadCastCommand;
+
+class Loader extends PluginBase {
+  
+   public static $prefix = '§2(Test';
+      
+   public  function onEnable(): void
+    {
+        $this->getServer()->getCommandMap()->register("/bc", new BroadCastCommand($this));
+}
+}
