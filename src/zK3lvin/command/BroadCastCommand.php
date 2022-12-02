8ea29f425c5bee9ec7a9d15d7e3f58573cc76789
zK3lvin <109186535+zK3lvin@users.noreply.github.com>
2022-12-01 22:20:19 -0600

    update BroadCastCommand.php

--- src/zK3lvin/command/BroadCastCommand.php
+++ src/zK3lvin/command/BroadCastCommand.php
@@ -1,1 +1,34 @@
+<?php
+
+namespace zK3lvin\command;
+
+use pocketmine\command\{Command, CommandSender};
+use pocketmine\Server;
+use zK3lvin\Loader;
+
+class BroadCaatCommand extends Command
+{
+
+    public function __construct()
+    {
+        parent::__construct("broadcast", "Broadcast message", "/broadcast <message>", ["bc"]);
+    }
+
+    public function execute(CommandSender $sender, string $label, array $args): void
+    {
+        if (count($args) < 1) {
+            $sender->sendMessage("§cUse /broadcast (mensaje)");
+            return;
+        }
+        $message = implode(" ", $args);
+        foreach ($this->getServer()->getOnlinePlayers() as $player) {
+            $player->sendMessage(Loader::$prefix . "§4" . $message);
+        }
+    }
+
+    public function getServer(): Server
+    {
+        return Server::getInstance();
+    }
+}
 
