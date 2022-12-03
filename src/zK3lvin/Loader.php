d621169640e9c114c5953d86a788b7f94f7ed60f
zK3lvin <109186535+zK3lvin@users.noreply.github.com>
2022-12-03 10:38:16 -0600

    update Loader.php

--- src/zK3lvin/Loader.php
+++ src/zK3lvin/Loader.php
@@ -7,9 +7,9 @@ use zK3lvin\command\BroadCastCommand;
 
 class Loader extends PluginBase {
   
-   public static $prefix = '§2(Test';
+   public static $prefix = '§2(Test)';
       
-   public  function onEnable(): void
+   public function onEnable(): void
     {
         $this->getServer()->getCommandMap()->register("/bc", new BroadCastCommand($this));
 }
